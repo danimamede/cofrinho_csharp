9922663863b96f69e5dff5d64d045ca964f2c250
danimamede <danielamamede93@gmail.com>
2022-11-10 11:14:35 -0300

    IDesenvolvimento inicial do meu principal

--- Cofrinho/Cofrinho/Program.cs
+++ Cofrinho/Cofrinho/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("COFRINHO DA DANI");
+        Console.WriteLine("Por favor digite a opção desejada:");
+
+        int selectMenu;
+
+        do
+        {
+            Console.WriteLine("1. Adicionar moeda\n2. Remover moeda\n3.Listar moedas no cofrinho\n4. Consultar valor total em reais\n5. Encerrar");
+            selectMenu = int.Parse(Console.ReadLine());
+
+            switch (selectMenu)
+            {
+                case 1:
+                    // adicionar moeda
+                    break;
+                case 2:
+                    // remover moeda
+                    break;
+                case 3:
+                    // listar moeda
+                    break;
+                case 4:
+                    // consultar valor total em reais
+                    break;
+                case 5:
+                    Console.WriteLine("\nEncerrando... Sistema finalizado.");
+                    break;
+                default:
+                    Console.WriteLine("\nPor favor escolha uma opção válida.");
+                    break;
+            }
+
+        } while (selectMenu != 5);
+    }
+}
